57fbd657eabea6999688b4ea0c5da8f460c23ad9
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-10-10 15:04:41 -0300

    footer create

--- psicologia-landingpage/src/App.jsx
+++ psicologia-landingpage/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import About from './components/About/About'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Services from './components/Services/Services'
@@ -13,6 +14,7 @@ function App() {
      <Hero/>
      <About/>
      <Services/>
+     <Footer/>
     </>
   )
 }
